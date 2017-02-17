ac30a96172e00581ae269ca82e093ca46f64f9d6
Zhaoyang <yiz24@uw.edu>
2017-02-17 14:40:41 -0800

    the setting is not correctly displayed

--- app/src/main/java/edu/washington/yiz24/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/yiz24/quizdroid/MainActivity.java
@@ -3,7 +3,6 @@ package edu.washington.yiz24.quizdroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.CascadingMenuPopup;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
